a2e39521d8e8e470de0caa7143693ab3614355fb
qqyemingqq <qqyemingqq@gamil.com>
2017-05-12 22:11:00 +0800

    add mp3 file dir

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {shell} = require('electron')
-console.log( )
+// const {shell} = require('electron')
+// console.log( )
 var input_ele = document.getElementById('url_input');
 console.log(input_ele);
 
@@ -13,3 +13,30 @@ function checkUrl(url) {
         shell.showItemInFolder('\\10.10.20.42\share')
     }
 }
+//本地文件写入
+var path = require('path');
+var _path = path.join(__dirname, '..', input_ele.value);
+// var path1 = "d:\\ProjectsSpace\\ElectronProjects\\ElectronTest2\\app\\html\\config\\record.txt";
+console.log(_path);//测试路径对不对的
+var fs = require('fs');
+// fs.readFile(_path, 'utf8', function (err, data) {
+// if (err) return console.log(err);
+// });
+
+// fs.writeFile(_path, "electron + Javascript", function (err) {
+// if (!err)
+//     console.log("写入成功！")
+// })
+fs.readdir('\\\\10.10.20.42\\Musics\\', function(err, files) {  
+    console.log(files)
+    var audio = document.getElementById('audio_source');
+    audio.setAttribute('src','http://yinyueshiting.baidu.com/data2/music/122873158/4904681494442861128.mp3')
+        // if (err) {  
+        //     console.log('read dir error');  
+        // } else {  
+        //     files.forEach(function(item) {  
+        //         console.log(item);
+        //     });
+        // }
+})
+
