060986bd167ca93b6305f1ae703e1cceb1201627
qqyemingqq <qqyemingqq@gamil.com>
2017-06-01 19:32:27 +0800

    no frame windows,no resize,movable

--- main.js
+++ main.js
@@ -8,7 +8,15 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});//,transparent: true,frame: false
+  win = new BrowserWindow({
+    width: 570,
+    height: 288,
+    transparent: true,
+    frame: false,
+    backgroundColor: '#0066CC',
+    resizable:false,
+    // thickFrame:false
+  });//,transparent: true,frame: false
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -18,7 +26,7 @@ function createWindow () {
   }))
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
